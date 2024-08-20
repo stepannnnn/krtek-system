346173654173957d72e5381dab07a10319edfd18
stepannnnn <stepan.koblizka@gmail.com>
2024-08-20 17:00:23 +0200

    add header and footer, prepare for basic UI

--- krteksystem/orders/urls.py
+++ krteksystem/orders/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "orders"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.guest_action, name="guest action"),
+    path("<int:pk>/", views.guest_action, name="guest_action"),
     path("history/", views.AllHistoryViev.as_view(), name="history"),
     path("<int:pk>/choose_item/", views.ChooseItemView.as_view(), name="choose_item"),
     path("<int:pk>/choose_item/ordering/", views.ordering, name="ordering"),
